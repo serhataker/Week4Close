bd23e4b33ab4e93c1de72fec405253f940a92f48
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-08 20:33:52 +0300

    Add project files.

---
+++ BaseMachine.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Week4Close
+{
+    public abstract class BaseMachine // we created the basemachine abstract class main project for the our machine
+    {
+        // Define the properties
+        public DateTime ProductDate { get; set; } 
+
+        public string SerialNumber { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string OperatingSystem { get; set; }
+
+        public BaseMachine() // constructor for the productdate
+        {
+            ProductDate = DateTime.Now;
+        }
+
+        public virtual void WriteInfo() // Pollymorfihisim for the project
+        {
+            Console.WriteLine($"Product Date:{ProductDate} Serial Number:{SerialNumber} Name:{Name} Description:{Description} Operation System:{OperatingSystem}");
+
+        }
+
+             public void MainInfo() // This method when our prouced is over we call
+        {
+            Console.WriteLine("Congratulations! You produced a product. Do you want to produce another product? (Y/N)");
+            char response = char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+            if (response != 'Y')// if user don't another produce
+            {
+                Console.WriteLine("Thank you for using our product creator. Goodbye!");
+                Environment.Exit(0);
+               
+            }
+        }
+
+        public abstract void GetProductName(); //we created the abstract method
+    }
+       public class Phone : BaseMachine // We cretaed the for Phone Class and inherit the basemachine class
+        {
+            public Boolean TurkishLicence { get; set; }// this propety special property for the Phone class
+            public override void GetProductName() // we override the abstract method and dizayn for our Phone object
+            {
+            Console.WriteLine($"Your Phone's Name: {Name.ToString()}");
+            }
+
+            public override void WriteInfo() // we override the abstract method and dizayn for our Phone object printing
+        {
+                Console.WriteLine($"Product Date:{ProductDate} Serial Number:{SerialNumber} Name:{Name} Description:{Description} Operation System:{OperatingSystem} KVK:{TurkishLicence}");
+            }
+        } 
+        
+        public class Computer : BaseMachine //We cretaed the for PC Class and inherit the basemachine class
+    {
+
+            public Boolean BluetoothIsHave { get; set; } // this propety special property for the PC class
+        private int _usbPortCount;// we created the field for the ncapsulation
+        public int UsbPortCount// we ncapsulate this property if user input wrong value
+        {
+            get { return _usbPortCount; }
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _usbPortCount = value;
+                }
+                else
+                {
+                    _usbPortCount = -1;
+                }
+            }
+        }
+
+
+
+
+        public override void GetProductName()   // we override the abstract method and dizayn for our PC object
+        
+            {
+            Console.WriteLine($"Your Computer's Name: {Name.ToString()}");
+        }
+
+        public override void WriteInfo() // we override the abstract method and dizayn for our PC object printing
+
+        {
+                Console.WriteLine($"Product Date:{ProductDate} Serial Number:{SerialNumber} Name:{Name} Description:{Description} Operation System:{OperatingSystem} Bluetooth:{BluetoothIsHave} Use Number :{_usbPortCount}");
+            }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Channels;
+using Week4Close;
+
+namespace Week4ForClose
+{
+
+    class Week4
+    {
+        static void Main(string[] args)
+        {
+            
+
+            do
+            {
+
+
+                Console.WriteLine("What do you Want to produce the product if you want to PC Click 1 or Phone Click 2"); //we ask the user which product you want to pruduce
+                string choose = (Console.ReadLine()); //we assign the variable which the user input value
+
+
+                switch (choose) // we controllled the variable
+                {
+
+                    case "1":// if choose  1 we created the PC machine and cretaed the property and call the method
+                        Computer pc = new Computer();
+                        Console.WriteLine("Please enter Pc Name");
+                        pc.Name = Console.ReadLine();
+                        Console.WriteLine("Please enter Serial Number");
+                        pc.SerialNumber = Console.ReadLine();
+                        Console.WriteLine("Please enter Pc's Description");
+                        pc.Description = Console.ReadLine();
+                        Console.WriteLine("Please enter Pc Operating System");
+                        pc.OperatingSystem = Console.ReadLine();
+                        Console.WriteLine("Please enter Pc Usb Port");
+                        pc.UsbPortCount = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Please enter Pc is have Bluetooth (True/False)");
+                        pc.BluetoothIsHave = Convert.ToBoolean(Console.ReadLine());
+                        pc.GetProductName();
+                        pc.WriteInfo();
+                        pc.MainInfo();
+                        break;
+
+
+                    case "2":// if choose  2 we created the Phone machine and cretaed the property and call the method
+                        Phone phone = new Phone(); 
+                        Console.WriteLine("Please enter Phone Name");
+                        phone.Name = Console.ReadLine();
+                        Console.WriteLine("Please enter Serial Number");
+                        phone.SerialNumber = Console.ReadLine();
+                        Console.WriteLine("Please enter Phone's Description");
+                        phone.Description = Console.ReadLine();
+                        Console.WriteLine("Please enter Phone Operating System");
+                        phone.OperatingSystem = Console.ReadLine();
+                        Console.WriteLine("Please enter Phone KVK (True/False)");
+                        phone.TurkishLicence = Convert.ToBoolean(Console.ReadLine());
+                        phone.GetProductName();
+                        phone.WriteInfo();
+                        phone.MainInfo();
+                        break;
+                        default:
+                        Console.WriteLine("please re enter 1 or 2"); //If user dont enter 1 or to we break and again we want to the user input 1 or 2
+                        break;
+                }
+            
+            } while (true);//we continuue if the MainInfo method return 'Y' value
+
+
+
+
+
+
+        }
+
+    }
+
+}
